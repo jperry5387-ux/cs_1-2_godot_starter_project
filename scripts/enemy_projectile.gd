705131db13e8b5323f2f7f630494412b77aeee1f
jperry5387-ux <jperry5387@student.pps.net>
2025-10-03 08:33:36 -0700

    made the fire shoot from my player

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+var direction
+var speed = 300
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
 	pass
+	
+	
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -10,7 +10,7 @@ func _process(delta: float) -> void:
 
 
 	
-
+func _on_
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	
