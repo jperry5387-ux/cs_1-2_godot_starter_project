5d45fa3cfe1729bf292212907ecbf09eb5790f1d
jperry5387-ux <jperry5387@student.pps.net>
2025-10-07 10:02:20 -0700

    i did somthing

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,8 @@ var facing = "down"
 var ySpeed = 300.0
 var yDirection = 0
 var coins = 0
+var is_attacking = false
+var attack_timer = .67
 @export var offset : Vector2 = Vector2(0, -25)
 
 # TODO: Add health system variables
@@ -16,8 +18,27 @@ var health = maxHealth
 
 func _ready() -> void:
 	pass
+func on_body_entered(body):
+
+	if body.is_in_group("enemy"):
+		pass
 
 func _physics_process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		is_attacking=true
+		if is_attacking:
+			attack_timer-=_delta
+			if attack_timer<0:
+				is_attacking=false
+				attack_timer=.67
+		
+
+
+
+
+
+
+
 	# TODO: Get horizontal input (left/right keys)
 	# Input.get_axis checks two keys and gives us a number:
 	# - When LEFT is pressed: returns -1.0
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,36 +1,46 @@
 extends CharacterBody2D
+var projectile_original = preload("res://scenes/enemy_projectile.tscn")
 var facing
 var start_time=2
 var time = start_time
 var in_range = false
 var player
-
+  
 func _ready():
 	
 	pass
 
 func _process(delta: float) -> void:
-	
+	if in_range == true:
+		time -= delta
+		if time < 0:
+			shoot(player)
+			time = start_time
 	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		player = body
 		in_range = true
-
+ 
+ 
 	
 
 pass
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name == "Player":
+		
+		in_range = false
 func shoot(target):
+	
 	var projectile_clone = projectile_original.instantiate()
 	
 	# TODO: Set projectile position to player position
-	projectile_clone.global_position = position + offset
+	projectile_clone.global_position = position
+	
 	
 	# TODO: Set projectile direction using facing variable
-	projectile_clone.set_direction(body.position)
+	projectile_clone.set_direction(target.position)
 	
 	# TODO: Add projectile to the game world
 	get_tree().get_root().add_child(projectile_clone)
