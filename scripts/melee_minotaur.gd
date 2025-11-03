3a8c6eb8eb999d06e792b7f3e97748d671f723bf
jperry5387-ux <jperry5387@student.pps.net>
2025-11-03 09:57:47 -0800

    worked

--- scripts/melee_minotaur.gd
+++ scripts/melee_minotaur.gd
@@ -3,7 +3,9 @@ extends Node
 var timer = 1
 var health = 10
 var maxHealth = 10
-
+var in_range = false
+var attacking = false
+var chasing = false
 func _process(delta: float) -> void:
 	timer -=delta
 	
@@ -16,3 +18,61 @@ func change_health(_amount:int):
 	if health > maxHealth:
 		health = maxHealth
 	print("Health: ", health)
+
+func _on_body_entered(delta: float) -> void:
+	pass
+
+
+
+func _on_melee_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+
+
+
+
+func _on_melee_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
+	if body.name == "player":
+		attacking = true
+		chasing = false
+
+
+
+
+
+
+
+func _on_chase_area_shape_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+	if body.name == "player":
+		attacking = true
+
+
+
+
+
+func _on_chase_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
+	if body.name == "player":
+		pass
+
+
+
+
+
+func _on_ranged_area_shape_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+	if body.name == "player":
+		pass
+
+
+
+
+
+
+func _on_ranged_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
+	if body.name == "player":
+		pass
